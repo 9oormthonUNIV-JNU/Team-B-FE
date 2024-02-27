0139a0ee8c8aec7048a64e025210927598a1a969
Gyeongmin <02dlrudals@gmail.com>
2024-02-27 23:13:55 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>gifty</p>
+        <p>test</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           Learn React
         </a>
       </header>
