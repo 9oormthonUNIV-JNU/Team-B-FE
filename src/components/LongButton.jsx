4b45ec6751f0448a28718ab2c8b1d3847505336f
juyeongnoh <juyeongnoh@gmail.com>
2024-02-28 23:50:03 +0900

    style: 긴 버튼 스타일링

--- src/components/LongButton.jsx
+++ src/components/LongButton.jsx
@@ -3,13 +3,29 @@ import styled from "styled-components";
 const StyledButton = styled.button`
   width: 100%;
   height: 56px;
+  border: 0;
+  font-size: 16px;
+  font-weight: 700;
   background-color: #ff7a00;
   border-radius: 9999px;
   color: #ffffff;
+  appearance: none;
+
+  &:disabled {
+    background-color: #8c8c8c;
+  }
+
+  &:active {
+    filter: brightness(75%);
+  }
 `;
 
-const LongButton = ({ label, onClick }) => {
-  return <StyledButton onClick={onClick}>{label}</StyledButton>;
+const LongButton = ({ label, onClick, ...props }) => {
+  return (
+    <StyledButton onClick={onClick} {...props}>
+      {label}
+    </StyledButton>
+  );
 };
 
 export default LongButton;
