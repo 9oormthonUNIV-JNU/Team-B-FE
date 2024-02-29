fd32b02232468424adb801ef86bcaa31983a10c9
juyeongnoh <juyeongnoh@gmail.com>
2024-02-29 22:38:14 +0900

    feat: 원형 이미지에 D-day prop 추가

--- src/components/CircledImage.jsx
+++ src/components/CircledImage.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+  position: relative;
+`;
+
+const Circle = styled.div`
   width: ${(props) => (props.width ? props.width + "px" : "56px")};
   height: ${(props) => (props.height ? props.height + "px" : "56px")};
   border-radius: 9999px;
@@ -14,10 +18,25 @@ const Image = styled.img`
   object-fit: cover;
 `;
 
-const CircledImage = ({ src, alt, width, height }) => {
+const Dday = styled.div`
+  position: absolute;
+  font-family: "Giants-Inline";
+  background-color: #ff7a00;
+  border-radius: 9999px;
+  padding: 0 8px;
+  color: #ffffff;
+  font-size: 14px;
+  bottom: 0;
+  right: 0;
+`;
+
+const CircledImage = ({ src, alt, width, height, dDay }) => {
   return (
-    <Wrapper width={width} height={height}>
-      <Image src={src} alt={alt} />
+    <Wrapper>
+      <Circle width={width} height={height}>
+        <Image src={src} alt={alt} />
+      </Circle>
+      {dDay && <Dday>D-{dDay}</Dday>}
     </Wrapper>
   );
 };
